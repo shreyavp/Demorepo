c60ff4104ad0ca54ff9ff9928bff123d9353485f
corwynnielsen <corwynnielsen@10.84.170.240>
2018-02-07 10:58:51 -0500

    Added 'Hello World' println.

--- src/demob1/democlass.java
+++ src/demob1/democlass.java
@@ -2,7 +2,7 @@ package demob1;
 
 public class democlass {
 public static void main(String[] args){
-	System.out.println("Edit this line");
+	System.out.println("Hello World!");
 	System.out.println("new line");
 }
 }
