18a13ad61516fa8142b302df87ffa29ca44a5101
yxue5 <yxue5@buffalo.edu>
2018-02-08 12:45:58 -0500

    Yang's edit

--- src/demob1/democlass.java
+++ src/demob1/democlass.java
@@ -2,7 +2,7 @@ package demob1;
 
 public class democlass {
 public static void main(String[] args){
-	System.out.println("Hello World!");
+	System.out.println("Yang's edit");
 	System.out.println("new line");
 }
 }
